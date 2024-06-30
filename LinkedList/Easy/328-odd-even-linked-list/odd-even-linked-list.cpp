8f58f70edb770745cff8a50bc902775d294dc846
Aniketdhar810 <143306304+Aniketdhar810@users.noreply.github.com>
2024-07-01 00:53:02 +0530

    Time: 12 ms (18.20%) | Memory: 14 MB (77.24%) - LeetSync

--- LinkedList/Easy/328-odd-even-linked-list/odd-even-linked-list.cpp
+++ LinkedList/Easy/328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -14,28 +14,40 @@ public:
         if(head==NULL || head->next==NULL){
             return head;
         }
-        vector<int> arr;
-        ListNode* temp=head;
-        // even indices
-        while(temp!=NULL && temp->next!=NULL){
-            arr.push_back(temp->val);
-            temp=temp->next->next;
+        // vector<int> arr;
+        // ListNode* temp=head;
+        // // even indices
+        // while(temp!=NULL && temp->next!=NULL){
+        //     arr.push_back(temp->val);
+        //     temp=temp->next->next;
+        // }
+        // //leftover elements
+        // if(temp) arr.push_back(temp->val);
+        // //odd indices
+        // temp=head->next;
+        // while(temp!=NULL && temp->next!=NULL){
+        //     arr.push_back(temp->val);
+        //     temp=temp->next->next;
+        // }
+        // if(temp) arr.push_back(temp->val);
+        // //pushing it to LL from arr
+        // temp=head;
+        // for(int i=0;i<arr.size();i++){
+        //     temp->val=arr[i];
+        //     temp=temp->next;
+        // }
+        // return head;
+        ListNode* odd=head;
+        ListNode* even=head->next;
+        ListNode* evenhead=head->next;
+        while(even!=NULL && even->next!=NULL){
+            odd->next=odd->next->next;
+            even->next=even->next->next;
+
+            odd=odd->next;
+            even=even->next;
         }
-        //leftover elements
-        if(temp) arr.push_back(temp->val);
-        //odd indices
-        temp=head->next;
-        while(temp!=NULL && temp->next!=NULL){
-            arr.push_back(temp->val);
-            temp=temp->next->next;
-        }
-        if(temp) arr.push_back(temp->val);
-        //pushing it to LL from arr
-        temp=head;
-        for(int i=0;i<arr.size();i++){
-            temp->val=arr[i];
-            temp=temp->next;
-        }
+        odd->next=evenhead;
         return head;
     }
 };
