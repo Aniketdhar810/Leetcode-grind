33f2293d923334d40acc6651caaade03cc2c33b3
Aniketdhar810 <143306304+Aniketdhar810@users.noreply.github.com>
2024-08-13 13:19:35 +0530

    Time: 4 ms (70.20%) | Memory: 12.1 MB (57.92%) - LeetSync

---
+++ POTD/496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        //first approach is compare every element to the right
+        //using stack
+        stack<int>st;
+        map<int,int>mp;
+        for(int i=nums2.size()-1;i>=0;i--){
+            while (!st.empty() && nums2[i] >= st.top()) {
+                st.pop();
+            }
+            if (st.empty()) {
+                mp[nums2[i]] = -1;
+            } else {
+                mp[nums2[i]] = st.top();
+            }
+            st.push(nums2[i]);
+        }
+        vector<int> result;
+        for (int num : nums1) {
+            result.push_back(mp[num]);
+        }
+        return result;
+    }
+};
