95cccaa7af98000cd72ef0c3517de2a46d4ac894
Aniketdhar810 <143306304+Aniketdhar810@users.noreply.github.com>
2024-08-28 22:01:04 +0530

    Time: 10 ms (48.81%) | Memory: 21.8 MB (56.39%) - LeetSync

--- Tree/110-balanced-binary-tree/balanced-binary-tree.cpp
+++ Tree/110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -11,23 +11,34 @@
  */
 class Solution {
 public:
-    int maxDepth(TreeNode* root) {
-        if(root==NULL) return 0;
-        int l=maxDepth(root->left);
-        int r=maxDepth(root->right);
-        return 1+max(l,r);
-    }
+    // int maxDepth(TreeNode* root) {
+    //     if(root==NULL) return 0;
+    //     int l=maxDepth(root->left);
+    //     int r=maxDepth(root->right);
+    //     return 1+max(l,r);
+    // }
+int dfsHeight (TreeNode *root) {
+    if (root == NULL) return 0;
+    int leftHeight = dfsHeight (root -> left);
+    if (leftHeight == -1) return -1;
+    int rightHeight = dfsHeight (root -> right);
+    if (rightHeight == -1) return -1;
+    if (abs(leftHeight - rightHeight) > 1) return -1;   
+    return max (leftHeight, rightHeight) + 1;
+
+}
     bool isBalanced(TreeNode* root) {
-        if (root == nullptr) {
-            return true;
-        }
-        int leftHeight = maxDepth(root->left);
-        int rightHeight = maxDepth(root->right);
-        if (abs(leftHeight - rightHeight) <= 1 &&
-            isBalanced(root->left) &&
-            isBalanced(root->right)) {
-            return true;
-        }
-        return false;
+        // if (root == nullptr) {
+        //     return true;
+        // }
+        // int leftHeight = maxDepth(root->left);
+        // int rightHeight = maxDepth(root->right);
+        // if (abs(leftHeight - rightHeight) <= 1 &&
+        //     isBalanced(root->left) &&
+        //     isBalanced(root->right)) {
+        //     return true;
+        // }
+        // return false;
+        return dfsHeight (root) != -1;
     }
 };
