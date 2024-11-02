ba3a8ae034b46133f58a90f2ee68236887c03db9
Aniketdhar810 <143306304+Aniketdhar810@users.noreply.github.com>
2024-11-02 22:52:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (83.91%) - LeetSync

--- Tree/2580-circular-sentence/circular-sentence.cpp
+++ Tree/2580-circular-sentence/circular-sentence.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isCircularSentence(string sentence) {
-        // Check if sentence is a single word
+
         if (sentence.front() != sentence.back()) {
             return false;
         }
