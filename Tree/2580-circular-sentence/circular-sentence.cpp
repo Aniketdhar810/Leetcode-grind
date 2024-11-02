f102b3dd236852025f07a23adaac6dd983eb3685
Aniketdhar810 <143306304+Aniketdhar810@users.noreply.github.com>
2024-11-02 22:51:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (59.78%) - LeetSync

--- Tree/2580-circular-sentence/circular-sentence.cpp
+++ Tree/2580-circular-sentence/circular-sentence.cpp
@@ -1,32 +1,14 @@
 class Solution {
 public:
     bool isCircularSentence(string sentence) {
-        vector<string>a;
-        string s;
-        stringstream ss(sentence);
-        while(getline(ss,s,' ')){
-            a.push_back(s);
+        // Check if sentence is a single word
+        if (sentence.front() != sentence.back()) {
+            return false;
         }
-        int size=a.size();
-        if(size==1){
-            int l=a[0].size()-1;
-            if(a[0][0]!=a[0][l]){
+
+        for (int i = 0; i < sentence.size(); i++) {
+            if (sentence[i] == ' ' && sentence[i - 1] != sentence[i + 1]) {
                 return false;
-            }
-            else{
-                return true;
-            }
-        }
-        for(int i=0;i<size-1;i++){
-            int n=a[i].size()-1;
-            if(a[i][n]!=a[i+1][0]){
-                return false;
-            }
-            if(i==size-2){
-                int m=a[size-1].size()-1;
-                if(a[size-1][m]!=a[0][0]){
-                    return false;
-                }
             }
         }
         return true;
