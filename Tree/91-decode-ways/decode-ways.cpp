3a428e8c146b898d46d41626405657c4b794e125
Aniket Dhar <143306304+Aniketdhar810@users.noreply.github.com>
2026-07-25 01:40:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (88.53%) - LeetSync

--- Tree/91-decode-ways/decode-ways.cpp
+++ Tree/91-decode-ways/decode-ways.cpp
@@ -12,20 +12,42 @@ public:
     //     }
     //     return res;
     // }
-    int rec(string& s,int i,vector<int>& dp){
-        if(i==s.size()) return 1;
-        if(s[i]=='0') return 0;
-        if(dp[i]!=-1) return dp[i];
-        int res=rec(s,i+1,dp);
-        if(i<s.size()-1){
-            if(s[i]=='1'|| (s[i]=='2' && s[i+1]<'7')){
-                res+=rec(s,i+2,dp);
+    // top down
+    // int rec(string& s,int i,vector<int>& dp){
+    //     if(i==s.size()) return 1;
+    //     if(s[i]=='0') return 0;
+    //     if(dp[i]!=-1) return dp[i];
+    //     int res=rec(s,i+1,dp);
+    //     if(i<s.size()-1){
+    //         if(s[i]=='1'|| (s[i]=='2' && s[i+1]<'7')){
+    //             res+=rec(s,i+2,dp);
+    //         }
+    //     }
+    //     return dp[i]=res;
+    // }
+    // Since dp[i] depends on larger indices, fill from right to left.
+    // bottom up
+    int rec(string& s,vector<int>& dp){
+        int n=s.size();
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='0'){
+                dp[i]=0;
+                continue;
+            }
+            dp[i]=dp[i+1];
+            if(i<n-1){
+                if(s[i]=='1'|| (s[i]=='2' && s[i+1]<'7')){
+                    dp[i]+=dp[i+2];
+                }
             }
         }
-        return dp[i]=res;
+        return dp[0];
     }
     int numDecodings(string s) {
-        vector<int>dp(s.size(),-1);
-        return rec(s,0,dp);
+        // vector<int>dp(s.size(),-1);
+        int n=s.size();
+        vector<int>dp(n+1,0);
+        return rec(s,dp);
     }
 };
