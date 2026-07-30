ec3f28db5f344a1db621fa9e73daea8e41cca665
Aniket Dhar <143306304+Aniketdhar810@users.noreply.github.com>
2026-07-30 12:52:08 +0530

    Time: 11 ms (56.12%) | Memory: 41.3 MB (15.08%) - LeetSync

--- Tree/494-target-sum/target-sum.cpp
+++ Tree/494-target-sum/target-sum.cpp
@@ -1,14 +1,38 @@
+// class Solution {
+// public:
+//     int ans=0;
+//     void rec(vector<int>& nums,int target,int index,int sum){
+//         if(index==nums.size() && sum==target){ans++; return;}
+//         else if(index==nums.size()) return;
+//         rec(nums,target,index+1,sum+nums[index]);
+//         rec(nums,target,index+1,sum-nums[index]);
+//     }
+//     int findTargetSumWays(vector<int>& nums, int target) {
+//         rec(nums,target,0,0);
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    int ans=0;
-    void rec(vector<int>& nums,int target,int index,int sum){
-        if(index==nums.size() && sum==target){ans++; return;}
-        else if(index==nums.size()) return;
-        rec(nums,target,index+1,sum+nums[index]);
-        rec(nums,target,index+1,sum-nums[index]);
+    vector<vector<int>> dp;
+
+    int rec(vector<int>& nums, int target, int index, int sum) {
+        if (index == nums.size()) {
+            return (sum == target) ? 1 : 0;
+        }
+
+        if (dp[index][sum + 1000] != -1)
+            return dp[index][sum + 1000];
+
+        int add = rec(nums, target, index + 1, sum + nums[index]);
+        int subtract = rec(nums, target, index + 1, sum - nums[index]);
+
+        return dp[index][sum + 1000] = add + subtract;
     }
+
     int findTargetSumWays(vector<int>& nums, int target) {
-        rec(nums,target,0,0);
-        return ans;
+        dp.resize(nums.size(), vector<int>(2001, -1));
+        return rec(nums, target, 0, 0);
     }
 };
