726ef8dd80e5fbe0e95fc1f1a583ed9feb7e6e29
Aniket Dhar <143306304+Aniketdhar810@users.noreply.github.com>
2026-08-01 16:36:57 +0530

    Time: 40 ms (16.06%) | Memory: 26.9 MB (72.65%) - LeetSync

--- Tree/115-distinct-subsequences/distinct-subsequences.cpp
+++ Tree/115-distinct-subsequences/distinct-subsequences.cpp
@@ -20,7 +20,8 @@ public:
         if(i==s.size()) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
         if(s[i]==t[j]) return dp[i][j]= rec(s,t,i+1,j+1,dp) + rec(s,t,i+1,j,dp);
-        return dp[i][j]=rec(s,t,i+1,j,dp); 
+        else dp[i][j]=rec(s,t,i+1,j,dp); 
+        return dp[i][j];
     }
     int numDistinct(string s, string t) {
         vector<vector<int>>dp(s.size(),vector<int>(t.size(),-1));
