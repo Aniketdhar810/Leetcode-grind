1c764792ab62d5cf1d8d372eb3e42403216faa5a
Aniket Dhar <143306304+Aniketdhar810@users.noreply.github.com>
2026-08-02 15:31:41 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (71.19%) - LeetSync

--- DP/300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ DP/300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(n,-1);
-        int maxi=1;
-        dp[0]=1;
-        for(int i=1;i<n;i++){
-            int add=0;
-            for(int j=0;j<i;j++){
-                if(nums[j]<nums[i]){
-                    add=max(add,dp[j]);
-                }
-            }
-            dp[i]=1+add;
-            maxi=max(maxi,dp[i]);
+        // int n=nums.size();
+        // vector<int>dp(n,-1);
+        // int maxi=1;
+        // dp[0]=1;
+        // for(int i=1;i<n;i++){
+        //     int add=0;
+        //     for(int j=0;j<i;j++){
+        //         if(nums[j]<nums[i]){
+        //             add=max(add,dp[j]);
+        //         }
+        //     }
+        //     dp[i]=1+add;
+        //     maxi=max(maxi,dp[i]);
+        // }
+        // return maxi;
+        vector<int>fred_again;
+        for(int x:nums){
+            auto it=lower_bound(fred_again.begin(),fred_again.end(),x);
+            if(it==fred_again.end()) fred_again.push_back(x);
+            else *it=x;
         }
-        return maxi;
+        return fred_again.size();
     }
 };
